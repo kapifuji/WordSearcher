0ca3d50d7f9ef3c7a389771005b99d77af0faf01
rpianna <kantarui619816@gmail.com>
2020-05-09 23:55:33 +0900

    アイコンを登録

--- Main.py
+++ Main.py
@@ -1,10 +1,18 @@
+import os
 import sys
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtGui
 from source.GUI import MainWindow
 import source.Const as Const
 
+
+def resource_path(relative) -> str:
+    if hasattr(sys, "_MEIPASS"):
+        return os.path.join(sys._MEIPASS, relative)
+    return os.path.join(relative)
+
 def main() -> None:
     app = QtWidgets.QApplication(sys.argv)
+    app.setWindowIcon(QtGui.QIcon(resource_path("icon.ico")))
     gui = MainWindow(Const.windowName)
     gui.show()
 
